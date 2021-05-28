67847c6420c9b9ed3f04bee651c82e033848bbed
RocketScience98 <luisalbertodavila@gmail.com>
2021-05-28 18:48:36 -0500

    done

---
+++ src/components/CountriesList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const CountriesList = (props) => {
+  const countryList = props.countries.map((country, index) => {
+    return (
+      <Link
+        key={index + 1}
+        to={country.cca3}
+        className="list-group-item list-group-item-action"
+      >
+        {country.flag} {country.name.common}
+      </Link>
+    );
+  });
+  return <div className="list-group">{countryList}</div>;
+};
+export default CountriesList;
